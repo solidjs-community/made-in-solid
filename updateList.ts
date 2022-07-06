d263c41397f779c4cc1a1327bb2c174ca9f854c8
Damian Tarnawski <gthetarnav@gmail.com>
2022-07-06 19:32:44 +0200

    fix image src

--- updateList.ts
+++ updateList.ts
@@ -10,40 +10,40 @@ const pathTo = (...path: string[]) => join(__dirname, ...path)
 const getCommentText = (text: string, marker: "START" | "END") => `<!-- ${text}:${marker} -->`
 
 function insertTextBetweenComments(file: string, text: string, comment: string): string {
-  const startComment = getCommentText(comment, "START")
-  const endComment = getCommentText(comment, "END")
-  const lines = file.split("\n")
-  const start = lines.findIndex(line => line.includes(startComment))
-  const end = lines.findIndex(line => line.includes(endComment), start + 1)
-  if (start === -1 || end === -1) throw `Could not find ${comment} in ${file}`
-  lines.splice(start + 1, end - start - 1, ...text.split("\n"))
-  return lines.join("\n")
+	const startComment = getCommentText(comment, "START")
+	const endComment = getCommentText(comment, "END")
+	const lines = file.split("\n")
+	const start = lines.findIndex(line => line.includes(startComment))
+	const end = lines.findIndex(line => line.includes(endComment), start + 1)
+	if (start === -1 || end === -1) throw `Could not find ${comment} in ${file}`
+	lines.splice(start + 1, end - start - 1, ...text.split("\n"))
+	return lines.join("\n")
 }
 
 async function fetchPreviewImage(
-  website: string,
-  repo: string | undefined
+	website: string,
+	repo: string | undefined,
 ): Promise<string | null> {
-  let preview = await getLinkPreview(website)
-  if ("images" in preview && preview.images.length) return preview.images[0]
-  if (repo) {
-    preview = await getLinkPreview(repo)
-    if ("images" in preview && preview.images.length) return preview.images[0]
-  }
-  return null
+	let preview = await getLinkPreview(website)
+	if ("images" in preview && preview.images.length) return preview.images[0]
+	if (repo) {
+		preview = await getLinkPreview(repo)
+		if ("images" in preview && preview.images.length) return preview.images[0]
+	}
+	return null
 }
 
 // PROGRAM
 
 ;(async () => {
-  const readmePath = pathTo("./README.md")
-  let readme = await readFile(readmePath, "utf8")
+	const readmePath = pathTo("./README.md")
+	let readme = await readFile(readmePath, "utf8")
 
-  const projects = data.map(async ({ name, description, website, repo }, i) => {
-    const preview = await fetchPreviewImage(website, repo)
-    return `---
+	const projects = data.map(async ({ name, description, website, repo }, i) => {
+		const preview = await fetchPreviewImage(website, repo)
+		return `---
 
-${preview ? `<a src=${preview} height="200" loading="lazy"/>` : ""}
+${preview ? `<a src="${preview}" height="200" loading="lazy"/>` : ""}
 
 ### ${name}
 
@@ -52,10 +52,10 @@ ${description}
 **Website:** [${website}](${website})
 
 ${repo ? `**Repository:** [${repo}](${repo})` : ""}`
-  })
-  const projectsText = (await Promise.all(projects)).join("\n\n")
+	})
+	const projectsText = (await Promise.all(projects)).join("\n\n")
 
-  readme = insertTextBetweenComments(readme, projectsText, "INSERT-PROJECTS")
+	readme = insertTextBetweenComments(readme, projectsText, "INSERT-PROJECTS")
 
-  await writeFile(readmePath, readme)
+	await writeFile(readmePath, readme)
 })()
