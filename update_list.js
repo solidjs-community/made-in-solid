f1fac6d50920cc1cba7daea65bef5c23a017e924
Damian Tarnawski <gthetarnav@gmail.com>
2024-02-21 16:39:26 +0100

    Remove link-preview-js dependency

--- update_list.js
+++ update_list.js
@@ -1,13 +1,14 @@
 import * as fsp          from "node:fs/promises"
 import * as path         from "node:path"
 import * as url          from "node:url"
-import * as link_preview from "link-preview-js"
-
 
 /**
- * @typedef {object} ImagePreview
- * @property {string | undefined} image
- * @property {string | undefined} desc
+ * @typedef {object} Project_Preview
+ * @property {string       } name
+ * @property {string       } desc
+ * @property {string       } link
+ * @property {string | null} repo
+ * @property {string | null} image
  *
  * @typedef {object} Project
  * @property {string            } name
@@ -20,17 +21,14 @@ const dirname     = path.dirname(url.fileURLToPath(import.meta.url))
 const readme_path = path.join(dirname, "./readme.md")
 const data_path   = path.join(dirname, "./projects.json")
 
-
 async function main() {
-	const readme_promise   = fsp.readFile(readme_path, "utf8")
-	const raw_data_promise = fsp.readFile(data_path, "utf8")
+	const readme_promise = fsp.readFile(readme_path, "utf8")
+	const raw_data = await fsp.readFile(data_path, "utf8")
 
-	let [readme, raw_data] = await Promise.all([readme_promise, raw_data_promise])
 	/** @type {Project[]} */
 	const projects = JSON.parse(raw_data)
-
-	const previews_promise = projects.map((project) => fetchPreviewImage(project.link, project.repo))
-	const previews = await Promise.all(previews_promise)
+	const preview_promises = projects.map(fetch_project_preview)
+	const previews = await Promise.all(preview_promises)
 
 	let projects_txt = ""
 	for (let i = 0; i < projects.length; i++) {
@@ -52,12 +50,147 @@ ${project.repo ? `**Repository:** [${project.repo}](${project.repo})` : ""}`
 		if (i < projects.length - 1) projects_txt += "\n\n"
 	}
 
+	let readme = await readme_promise
 	readme = insertTextBetweenComments(readme, projects_txt, "INSERT-PROJECTS")
 
 	fsp.writeFile(readme_path, readme)
 
 	/* Write back to projects.json to format it */
 	fsp.writeFile(data_path, JSON.stringify(projects, null, "\t"))
+}
+
+/**
+ * @param   {string} html
+ * @param   {string} name
+ * @returns {string | null} */
+function get_meta_tag_content(html, name) {
+	const pattern_1 = `name="${name}"`
+	const pattern_2 = `property="${name}"`
+
+	/** @type {number} */
+	let start = 0
+
+	while (true) {
+		start = html.indexOf("meta", start)
+		if (start === -1) break
+		start += 4
+
+		let end = html.indexOf(">", start)
+		if (end === -1) break
+
+		let tag_text = html.slice(start, end)
+		if (!tag_text.includes(pattern_1) && !tag_text.includes(pattern_2)) continue
+
+		start = tag_text.indexOf('content="')
+		if (start === -1) continue
+
+		start += 9
+		end = tag_text.indexOf('"', start)
+		if (end === -1) continue
+
+		return tag_text.slice(start, end)
+	}
+
+	return null
+}
+
+/**
+ * @param   {string} html
+ * @param   {string} project_link
+ * @returns {string | null} */
+function get_image_link(html, project_link) {
+	let image
+		= get_meta_tag_content(html, "og:image")
+		?? get_meta_tag_content(html, "twitter:image")
+		?? get_meta_tag_content(html, "twitter:image:src")
+
+	if (!image) return null
+
+	image = decodeURIComponent(image)
+	image = image.replace(/&amp;/g, "&")
+
+	if (!image.startsWith("http")) {
+		image = new URL(image, project_link).href
+	}
+
+	return image
+}
+
+/**
+ * @param   {string} html
+ * @returns {string | null} */
+function get_title_tag(html) {
+	let start = html.indexOf("<title>")
+	if (start === -1) return null
+	start += 7
+
+	let end = html.indexOf("</title>", start)
+	if (end === -1) return null
+
+	return html.slice(start, end)
+}
+
+const FETCH_HTML_OPTIONS = /** @type {const} */({
+	headers: {
+		"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+		accept: "text/html",
+	}
+})
+
+/**
+ * @param   {string} url
+ * @returns {Promise<string | Error>} */
+function fetch_html(url) {
+	return fetch(url, FETCH_HTML_OPTIONS)
+		.then(res => res.text())
+		.catch(error => error)
+}
+
+/**
+ * @param   {Project} project
+ * @returns {Promise<Project_Preview>} */
+async function fetch_project_preview(project) {
+	/** @type {Project_Preview} */
+	const preview = {
+		name: project.name,
+		desc: project.desc,
+		link: project.link,
+		repo: project.repo ?? null,
+		image: null,
+	}
+
+	let html = await fetch_html(project.link)
+
+	if (typeof html === "string") {
+		preview.name
+			= get_meta_tag_content(html, "og:site_name")
+			?? get_title_tag(html)
+			?? get_meta_tag_content(html, "og:title")
+			?? get_meta_tag_content(html, "twitter:title")
+			?? preview.name
+
+		preview.image = get_image_link(html, project.link)
+
+		preview.desc
+			=  get_meta_tag_content(html, "og:description")
+			?? get_meta_tag_content(html, "description")
+			?? get_meta_tag_content(html, "twitter:description")
+			?? preview.desc
+	} else {
+		console.error(`Error fetching ${project.link}: ${html.message}`)
+	}
+
+	if (project.repo && !preview.image) {
+		html = await fetch_html(project.repo)
+
+		if (typeof html === "string") {
+			preview.image = get_image_link(html, project.link)
+		} else {
+			console.error(`Error fetching ${project.repo}: ${html.message}`)
+		}
+	}
+
+	return preview
 }
 
 /**
@@ -82,40 +215,6 @@ function insertTextBetweenComments(file, text, comment) {
 	if (start === -1 || end === -1) throw `Could not find ${comment} in ${file}`
 	lines.splice(start + 1, end - start - 1, ...text.split("\n"))
 	return lines.join("\n")
-}
-
-const GET_LINK_PREVIEW_OPTIONS = /** @type {const} */({followRedirects: "follow"})
-
-/**
- * @param   {string               } website
- * @param   {string | undefined   } repo
- * @returns {Promise<ImagePreview>} */
-async function fetchPreviewImage(website, repo) {
-	try {
-		let preview = await link_preview.getLinkPreview(website, GET_LINK_PREVIEW_OPTIONS)
-		/** @type {string | undefined} */
-		let description
-
-		if ("images" in preview && preview.images.length)
-			return {image: preview.images[0], desc: preview.description}
-
-		if ("description" in preview && preview.description) {
-			description = preview.description
-		}
-		if (repo) {
-			preview = await link_preview.getLinkPreview(repo, GET_LINK_PREVIEW_OPTIONS)
-			if ("description" in preview && preview.description) description = preview.description
-			if ("images" in preview && preview.images.length)
-				return {
-					image: preview.images[0],
-					desc: description,
-				}
-		}
-		return {image: undefined, desc: description}
-	} catch (error) {
-		console.error({ error, website, repo })
-		return {image: undefined, desc: undefined}
-	}
 }
 
 
